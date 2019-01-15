689f380ef39e2e583e3a5d526ebcd4b8f0e5bae2
Ehmad Zubair <ezubair@gmail.com>
2019-01-15 18:22:41 +0500

    Added requirements.txt and Subject Admin

--- subjectnotes/notes/admin.py
+++ subjectnotes/notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subject
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ['id', 'title']
+
+admin.site.register(Subject, SubjectAdmin)
