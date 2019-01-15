43fd56e7b1e9bfe513a8f6d7bc5e27ce218fd487
Ehmad Zubair <ezubair@gmail.com>
2019-01-15 20:42:28 +0500

    Added Note Admin

--- subjectnotes/notes/admin.py
+++ subjectnotes/notes/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Subject
+from .models import Subject, Note
 
 
 class SubjectAdmin(admin.ModelAdmin):
     class Meta:
         list_display = ['id', 'title']
 
+
+class NoteAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ['id', 'subject', 'title']
+
+
 admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Note, NoteAdmin)
