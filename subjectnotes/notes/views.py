d8b412cdc43a85ba6f4829a14e2fdb61609f547a
ahmadRazaa <ahmad.raza@arbisoft.com>
2019-01-17 12:34:43 +0500

    added subject list

--- subjectnotes/notes/views.py
+++ subjectnotes/notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Subject
 
-# Create your views here.
+
+def subject_list(request):
+    subjects = Subject.objects.all()
+
+    return render(request, 'subject_list.html', {'subjects': subjects})
