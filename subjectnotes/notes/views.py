cd2f6002575d3dbcb566362e8a5d25daaf3026f8
ahmadRazaa <ahmad.raza@arbisoft.com>
2019-01-17 12:48:32 +0500

    added notes list

--- subjectnotes/notes/views.py
+++ subjectnotes/notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Subject
 
 
@@ -6,3 +6,10 @@ def subject_list(request):
     subjects = Subject.objects.all()
 
     return render(request, 'subject_list.html', {'subjects': subjects})
+
+
+def note_list(request, subject_id):
+    subject = get_object_or_404(Subject, pk=subject_id)
+    notes = subject.note_set.all()
+
+    return render(request, 'note_list.html', {'notes': notes})
--- subjectnotes/subjectnotes/urls.py
+++ subjectnotes/subjectnotes/urls.py
@@ -5,4 +5,5 @@ from notes import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subject_list/', views.subject_list, name='subject_list'),
+    path('note_list/<int:subject_id>/', views.note_list, name='note_list'),
 ]
